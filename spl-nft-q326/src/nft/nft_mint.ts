7fcf3f389c3bb2d48759d49531b834d3ddb9cfbc
DebasritaS <sahadebasrita0@gmail.com>
2026-08-31 12:22:21 +0530

    Update nft_mint.ts

--- spl-nft-q326/src/nft/nft_mint.ts
+++ spl-nft-q326/src/nft/nft_mint.ts
@@ -26,11 +26,17 @@ umi.use(mplCore());
     const asset = generateSigner(umi);
 
     //add you nft name and metadata uri
-    // const tx = await create()
+     const tx = await create(umi, {
+      asset,
+      name: "Debrug",
+      uri: metadataUri,
+    });
+    
+    const result = await tx.sendAndConfirm(umi);
 
-    // const signature = base58.deserialize(tx.signature)[0];
+    const signature = base58.deserialize(result.signature)[0];
 
-    // console.log(`signature ${signature} , asset : ${asset.publicKey}`);
+     console.log(`signature ${signature} , asset : ${asset.publicKey}`);
   } catch (e) {
     console.log(`errior ${e}`);
   }
